81a6b2fbc28c299a20242009c4e182f7df163f78
HenryRDavis <rainfallsprings@gmail.com>
2020-08-17 18:23:34 -0500

    did some things

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useParams } from 'react-router-dom'
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
+  const { id } = useParams();
+  // const { url, path } = useRouteMatch();
  
   useEffect(() => {
-    const id = 1;
+    
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -18,17 +21,18 @@ const Movie = (props) => {
           console.error(error);
         });
 
-  },[]);
+  }, [movie]);
   
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = evt => {
   // }
 
   if (!movie) {
-    return <div>Loading movie information...</div>;
+    return <div>Wait bro, just wait...</div>;
   }
 
   const { title, director, metascore, stars } = movie;
+
   return (
     <div className="save-wrapper">
       <div className="movie-card">
